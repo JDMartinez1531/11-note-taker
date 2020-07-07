3a28a98afbc01f3fa85cd0f6ad4da0a1f870da7f
JDMartinez1531 <63159799+JDMartinez1531@users.noreply.github.com>
2020-07-07 14:27:56 -0500

    built express server

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const express = require('express')
+// import necessary modules
 
+const express = require('express')
+var path = require("path");
+
+// Set up the Express App
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
